c1660920171d3e09e920188b01f6fbfcd7ef1c9c
Charles Davenport <cdavenport090@gmail.com>
2021-10-07 18:24:54 -0400

    compute a single laser reflection

--- VectorMath/BouncingLaser.gd
+++ VectorMath/BouncingLaser.gd
@@ -7,6 +7,7 @@ export var cast_length: float = 10.0
 var laser: Spatial
 var laser_line: Node
 var normal_line: Node
+var tangent_line: Node
 
 func _init():
 	if not Engine.editor_hint:
@@ -15,18 +16,21 @@ func _init():
 	set_physics_process(true)
 	laser = get_node("Laser")
 	laser_line = get_node("LaserLine")
-	normal_line = get_node("SphereNormalLine")
+	normal_line = get_node("NormalLine")
+	tangent_line = get_node("TangentLine")
 
 func _ready():
 	laser = get_node("Laser")
 	laser_line = get_node("LaserLine")
-	normal_line = get_node("SphereNormalLine")
+	normal_line = get_node("NormalLine")
+	tangent_line = get_node("TangentLine")
 
 func _physics_process(delta):
 	var space_state = get_world().direct_space_state
 	shoot_laser(space_state)
 
 func shoot_laser(space_state):
+	# initial raycast
 	laser_line.points.clear()
 	laser_line.points.append(laser.transform.origin)
 	var raycastStart = laser.transform.origin
@@ -36,12 +40,25 @@ func shoot_laser(space_state):
 	normal_line.points.clear()
 	if not hit:
 		normal_line.visible = false
+		tangent_line.visible = false
 		return
+	# draw normal vector on sphere, if hit
 	normal_line.visible = true
+	tangent_line.visible = true
 	normal_line.points.append(hit.position)
-	var normal_to = hit.position + hit.normal
-	normal_line.points.append(normal_to)
-	for i in range(1, max_bounces):
-		raycastStart = hit.position
+	normal_line.points.append(hit.position + hit.normal)
+	# compute the tangent vector to the hit surface
+	var incident = (raycastEnd - raycastStart)
+	var cross_vec = incident.cross(hit.normal)
+	var tangent_vec = hit.normal.cross(cross_vec).normalized()
+	# draw tangent vec
+	tangent_line.points.clear()
+	tangent_line.points.append(hit.position)
+	tangent_line.points.append(hit.position + tangent_vec)
+	# compute reflected vec
+	var reflected_vec = tangent_vec * (incident.dot(tangent_vec)) + hit.normal * (-1 * incident.dot(hit.normal))
+	laser_line.points.append(hit.position + reflected_vec.normalized() * cast_length)
+#	for i in range(1, max_bounces):
+#		raycastStart = hit.position
 		
 
