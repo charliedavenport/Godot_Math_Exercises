b0040b5d5259a54a36cb9d2946c3b97dfcff9eb6
Charles Davenport <cdavenport090@gmail.com>
2021-10-07 20:06:36 -0400

    many bounces

--- VectorMath/BouncingLaser.gd
+++ VectorMath/BouncingLaser.gd
@@ -48,7 +48,7 @@ func shoot_laser(space_state):
 	normal_line.points.append(hit.position)
 	normal_line.points.append(hit.position + hit.normal)
 	# compute the tangent vector to the hit surface
-	var incident = (raycastEnd - raycastStart)
+	var incident = raycastEnd - raycastStart
 	var cross_vec = incident.cross(hit.normal)
 	var tangent_vec = hit.normal.cross(cross_vec).normalized()
 	# draw tangent vec
@@ -57,8 +57,16 @@ func shoot_laser(space_state):
 	tangent_line.points.append(hit.position + tangent_vec)
 	# compute reflected vec
 	var reflected_vec = tangent_vec * (incident.dot(tangent_vec)) + hit.normal * (-1 * incident.dot(hit.normal))
-	laser_line.points.append(hit.position + reflected_vec.normalized() * cast_length)
-#	for i in range(1, max_bounces):
-#		raycastStart = hit.position
-		
+	#laser_line.points.append(hit.position + reflected_vec.normalized() * cast_length)
+	for i in range(1, max_bounces):
+		raycastStart = hit.position
+		raycastEnd = hit.position + reflected_vec.normalized() * cast_length
+		hit = space_state.intersect_ray(raycastStart, raycastEnd)
+		laser_line.points.append(hit.position if hit else raycastEnd)
+		if not hit:
+			return
+		incident = raycastEnd - raycastStart
+		cross_vec = incident.cross(hit.normal)
+		tangent_vec = hit.normal.cross(cross_vec).normalized()
+		reflected_vec = tangent_vec * (incident.dot(tangent_vec)) + hit.normal * (-1 * incident.dot(hit.normal))
 
